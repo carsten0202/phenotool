73c7480a67f9cb25db0936fb18c0dca375d41262
Carsten Rundsten <carsten.rundsten@sund.ku.dk>
2023-08-09 16:07:16 +0200

    v0.12.1

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -1,5 +1,5 @@
 
-from cli.version import __version__
-from cli.phenotool import main as Phenotool
-from cli.ukbiobank import ukbiobank as UKBiobank
+from .version import __version__
+from .phenotool import main as Phenotool
+from .ukbiobank import ukbiobank as UKBiobank
 
